be73cdb6707318b50b31c922d73ec09f1f8f351a
samaraneves <sneves0@hotmail.com>
2020-04-04 23:23:54 -0300

    Atualizar validação

--- js/efects.js
+++ js/efects.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
         const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (nome.length < 1) {
-            $('#camp-obrigatorio').css('display', 'block');
+            $('.camp-obrigatorio').css('display', 'block');
 
         } else if (nome != regexNome) {
-            $('#camp-tipo').css('display', 'block');
+            $('.camp-tipo').css('display', 'block');
         } else if (email.length == 0) {
-            $('#camp-obrigatorio').css('display', 'block');
+            $('.camp-obrigatorio').css('display', 'block');
         } else if (email != regexEmail) {
-            $('#camp-tipo').css('display', 'block');
+            $('.camp-tipo').css('display', 'block');
         }
 
     }

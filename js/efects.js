fd71cd15ba312ddfa16802de5769004354cb31ad
samaraneves <sneves0@hotmail.com>
2020-04-04 22:54:00 -0300

    Início da validação

--- js/efects.js
+++ js/efects.js
@@ -1,31 +1,20 @@
 $(document).ready(function() {
+    cadastro.onsubmit = function() {
+        const nome = document.getElementById('nome').value;
+        const regexNome = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/;
+        const email = document.getElementById('email').value;
+        const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    /*Animar página*/
-    $(window).on('scroll', function() {
-        var scrollDist = $(this).scrollTop(); /*Pegar a altura do scroll da página, mais o cálculo da sua proximidade da sua proximidade*/
-        $('[data-anime]').each(function() {
-            if (scrollDist > $(this).scrollTop()) {
-                $(this).addClass('anime');
-            } else {
-                $(this).removeClass('anime');
-            }
-        });
+        if (nome.length < 1) {
+            $('#camp-obrigatorio').css('display', 'block');
 
-    });
+        } else if (nome != regexNome) {
+            $('#camp-tipo').css('display', 'block');
+        } else if (email.length == 0) {
+            $('#camp-obrigatorio').css('display', 'block');
+        } else if (email != regexEmail) {
+            $('#camp-tipo').css('display', 'block');
+        }
 
-    /*Scroll suave ao clicar nos links do menu*/
-
-    $('nav a').click(function(e) {
-        /* Para que ao clicar em uma opção do menu, não apareça o nome 
-         * da âncora associada a section selecionada */
-    });
-
-
-    /*Validar formulário*/
-
-
-
-
-
-
+    }
 });
